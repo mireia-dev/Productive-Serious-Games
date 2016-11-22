1c049b4c7207540e003af24dc73fa5eb5307162a
Mireia <Mireia@mire_asus>
2016-11-22 16:55:28 +0100

    Añadida la parte de Vista de Administración

---
+++ src/java/org/primefaces/showcase/view/input/CalendarView.java
@@ -0,0 +1,48 @@
+package org.primefaces.showcase.view.input;
+ 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+ 
+@ManagedBean
+public class CalendarView {
+         
+    private Date fechaInicio;
+    private Date fechaFin;
+// <h:inputText type="date" name="fechamin" min="2016-01-01" value="2016-01-01" requiered="requiered"></h:inputText>-->
+//tendría que validar la fecha indicando su fecha mínimia, etc.
+    
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+     
+    public void click() {
+        
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+ 
+
+}
+
